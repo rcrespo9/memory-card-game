7d72f3074a84d6a6a1e652b9520ed2f96c18f574
rudycrespo <rudy.crespo20@gmail.com>
2017-07-01 12:24:52 -0400

    started on js, added header file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+(function() {
+	function init() {
+		console.log('Ready!');
+	}
+
+	function ready(fn) {
+	  if (document.readyState != 'loading'){
+	    fn();
+	  } else {
+	    document.addEventListener('DOMContentLoaded', fn);
+	  }
+	}
+
+	ready(init);
+})();
